366173ada443cd8b8de0a4cce70d54ca538b108e
simplettton <515315183@qq.com>
2017-08-19 14:11:01 +0800

    Initial Commit

---
+++ AirWave/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AirWave
+//
+//  Created by Macmini on 2017/8/19.
+//  Copyright © 2017年 Shenzhen Lifotronic Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AirWave/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AirWave
+//
+//  Created by Macmini on 2017/8/19.
+//  Copyright © 2017年 Shenzhen Lifotronic Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
